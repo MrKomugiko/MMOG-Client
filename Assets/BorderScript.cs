c2303835475631488d59eb4ae0db19fc88f31ce4
Kamil Mąka <Mr.Komugiko@gmail.com>
2021-05-30 14:25:45 +0100

    dodanie obramowania do lokalnego gracza ( zeby sie nie zgubic za sciana :d )

---
+++ Assets/BorderScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderScript : MonoBehaviour
+{
+    Transform transform;    
+    PlayerManager player;
+    Vector3Int borderPosition;
+
+    public Vector3Int BorderPosition 
+    {
+        get => borderPosition; 
+        set 
+        {
+       
+            borderPosition = value; 
+        }
+    }
+
+    private void Awake() {
+        SRenderer= GetComponent<SpriteRenderer>();
+        transform = GetComponent<Transform>();    
+        player = GetComponentInParent<PlayerManager>();
+    }
+    private void Start() {
+        ShowBorder();
+    }
+    void Update()
+    {
+        if(PositionChangeChangeCheck() == false) return;
+
+        transform.position = GameManager.instance._tileMap.CellToWorld(player.CurrentPosition_GRID);
+        BorderPosition = player.CurrentPosition_GRID;
+    }
+
+    bool PositionChangeChangeCheck(){
+        if(BorderPosition == player.CurrentPosition_GRID) return false;
+        return true;
+     }
+
+SpriteRenderer SRenderer;
+    public void ShowBorder()
+    {
+        SRenderer.enabled = true;
+    }
+    public void HideBorder()
+    {
+        SRenderer.enabled = false;
+    }
+    
+}
